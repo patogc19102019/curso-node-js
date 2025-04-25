cdc3c5df9b73ba3fba5b5e052eefd093a7c6d775
patogc19102019 <patricio.tapia33@inacapmail.cl>
2025-04-24 20:18:19 -0400

    mejora de red-file modo asincronico

---
+++ os-read-file.js
@@ -0,0 +1,15 @@
+const fs=require('node:fs')
+
+console.log('leyendo el primer archivo')
+fs.readFile('./archivo.txt','utf-8', (err,text)=>{
+    console.log(text)
+
+})
+console.log('hacer cosas mientras se lee el archivo')
+
+
+console.log('leyendo el segundo archivo')
+fs.readFile('./archivo2.txt','utf-8', (err,text)=>{
+    console.log(text)
+})
+
